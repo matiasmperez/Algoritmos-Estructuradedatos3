54e471d9ae68a6e09ab8c90f840c2c49dd9ab7af
FrancoBarraza09 <Francobarrazaboca@gmail.com>
2024-11-05 19:28:09 -0300

    agregado de portada

--- src/assets/components/Portada/Portada.jsx
+++ src/assets/components/Portada/Portada.jsx
@@ -1,6 +1,18 @@
 import "./Portada.css";
 function Portada() {
-  return <>{/* Escriban su contenido acá */}</>;
+  return <>{/* Escriban su contenido acá */}
+  <title>ISETA</title>
+
+  <body>
+    <div class="container">
+        <h1>Bienvenido Iseta</h1>
+        <p>Bienvenido al aula virtual de ISETA</p>
+        <a href="#" class="button">Descubre Más</a>
+    </div>
+</body>
+  
+  </>;
 }
 
 export default Portada;
+
