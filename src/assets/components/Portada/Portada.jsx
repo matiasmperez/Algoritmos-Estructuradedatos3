5eaf4771a2b8ae8e24977b35d952dd7ee67c486d
Matias Perez <ma7iasperez@gmail.com>
2024-11-05 20:08:32 -0300

    agregado portada

--- src/assets/components/Portada/Portada.jsx
+++ src/assets/components/Portada/Portada.jsx
@@ -1,18 +1,22 @@
 import "./Portada.css";
 function Portada() {
-  return <>{/* Escriban su contenido acá */}
-  <title>ISETA</title>
+  return (
+    <>
+      {/* Escriban su contenido acá */}
+      <title>ISETA</title>
 
-  <body>
-    <div class="container">
-        <h1>Bienvenido Iseta</h1>
-        <p>Bienvenido al aula virtual de ISETA</p>
-        <a href="#" class="button">Descubre Más</a>
-    </div>
-</body>
-  
-  </>;
+      <div className="container">
+        <div className="overlay"></div>
+        <h1 className="titulo2">
+          Bienvenido Algoritmos y Estructura de Datos III
+        </h1>
+        <p className="subtitulo2">Bienvenido a la página web de la materia</p>
+        <a href="#" className="button">
+          Descubre Más
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Portada;
-
