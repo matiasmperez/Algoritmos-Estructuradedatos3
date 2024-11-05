423b6b68d1691034372e5a044ced59ad9a5da1bd
sebastiaqn seisdedos <vsscmt@gmail.com>
2024-11-05 19:08:09 -0300

    Agregado de componente de profesores

--- src/assets/components/Profesores/Profesores.jsx
+++ src/assets/components/Profesores/Profesores.jsx
@@ -1,6 +1,24 @@
 import "./Profesores.css";
+import imagen from "./foto.jpg"
+
 function Profesores() {
-  return <>{/* Escriban su contenido acá */}</>;
+  const teachers = [
+    { name: "Matías Pérez", subject: "Estructura y Algoritmos III" },
+  ];
+
+  return (
+    <div className="profesores-container">
+      <h1>Profesores</h1>
+      <ul>
+        {teachers.map((teacher, index) => (
+          <li key={index}>
+            {teacher.name} - {teacher.subject}
+          </li>
+        ))}
+      </ul>
+      <img src={imagen} alt="Descripción de la imagen" />
+    </div>
+  );
 }
 
 export default Profesores;
